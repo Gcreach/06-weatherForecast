5e9523924725e1c1a7c9947762251ff225816e4f
Gcreach <gcreach97@gmail.com>
2023-04-27 18:26:53 -0700

    Updated html and js

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # 06-weatherForecast
+
+## Description
+
+This weather app will give you the details for the weather of a specific city
+
+## Installation
+
+Click the link to use this site: https://gcreach.github.io/06-weatherForecast/
+
+![Weather forecast app](./assets/weather-forecast.PNG)
+
+## Usage
+
+Type the name of the city in the search bar and click submit and see the results of your search. You will be given the temperature, wind, and humidity for the city along with a 5 day forecast.
+
+## Credits
+
+N/A
+
+## License
+
+Please refer to the LICENSE in the repo.
--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,19 @@
-var citySearchEl = document.querySelector(".city-search");
-var weatherDisplayEl = document.querySelector(".weather-display");
-var fiveDaysEl = document.querySelector(".five-days");
-var searchButton = document.querySelector(".search-button");
+var citySearchEl = document.querySelector("#city-search");
+var weatherDisplayEl = document.querySelector("#weather-display");
+var fiveDaysEl = document.querySelector("#five-days");
+var searchButton = document.querySelector("#btn");
+var latitude = 32.715736;
+var longitude = -117.161087;
+
+
+
+getWeatherApi();
+
+getGeoApi();
 
 function getWeatherApi() {
 
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}';
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=87e65aeff8380c1792c5b0b66b79ef3d';
 
     console.log(requestUrl);
     fetch(requestUrl)
@@ -13,11 +21,88 @@ function getWeatherApi() {
             return response.json();
         })
         .then(function (data) {
-            for (var i = 0; i <data.length; i++) {
-                var showWeather = document
-            }
+           console.log(data);     
+                var cityEl = document.createElement('a');
+                cityEl.classList = 'flex-row justify-space-between align-center';
+                cityEl.setAttribute('h5', data.name);
+                cityEl.setAttribute('p', data.temp);
+                cityEl.setAttribute('p', data.wind);
+                cityEl.setAttribute('p', data.humidity);
+        
         })
 }
 
-searchButton.addEventListener('click', getWeatherApi());
+function getGeoApi() {
+
+    var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=Phoenix&appid=87e65aeff8380c1792c5b0b66b79ef3d';
+
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+           console.log(data[0].lat);
+           console.log(data[0].lon);
+           var lat = data[0].lat;
+           var lon = data[0].lon;
+
+           var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=87e65aeff8380c1792c5b0b66b79ef3d';
+
+           console.log(requestUrl);
+           fetch(requestUrl)
+               .then(function (response) {
+                   return response.json();
+               })
+               .then(function (data) {
+                  console.log(data);
+                 
+               })
+               // Enter current weather API here
+
+        })
+}
+
+function getFiveDaysApi() {
+
+    var requestUrl = 'api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=87e65aeff8380c1792c5b0b66b79ef3d';
+
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+           console.log(data[0].lat);
+           console.log(data[0].lon);
+           var lat = data[0].lat;
+           var lon = data[0].lon;
+
+           var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=87e65aeff8380c1792c5b0b66b79ef3d';
+
+           console.log(requestUrl);
+           fetch(requestUrl)
+               .then(function (response) {
+                   return response.json();
+               })
+               .then(function (data) {
+                  console.log(data);
+
+                  for (var i = 0; i < 5; i++) {
+                    var cityEl = document.createElement('a');
+                    cityEl.classList = 'flex-row justify-space-between align-center';
+                    cityEl.setAttribute('h5', data.name);
+                    cityEl.setAttribute('p', data.temp);
+                    cityEl.setAttribute('p', data.wind);
+                    cityEl.setAttribute('p', data.humidity);
+                }
+                 
+               })
+               // Enter current weather API here
+
+        })
+}
+
+
+//searchButton.addEventListener("click", getWeatherApi);
 
